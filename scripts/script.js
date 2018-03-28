35bd50e7380a1fb7bd23bf0cb6f10caec274ead8
Unknown <nokid_gr@hotmail.it>
2018-03-29 00:37:19 +0300

    Javascript refactoring and cleaning up

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,6 @@ $(document).ready(function() {
        $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 1000);
     });
 
-    $('.js--scroll-to-start').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-    });
-
 
     /* Navigation scroll */
     $(function() {
@@ -32,7 +28,7 @@ $(document).ready(function() {
           if (target.length) {
             $('html,body').animate({
               scrollTop: target.offset().top
-            }, 1000);
+            }, 500);
             return false;
           }
         }
@@ -41,31 +37,16 @@ $(document).ready(function() {
 
 
     /* Animations on scroll */
+    /* Waypoint 1 */
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeInLeft');
     }, {
         offset: '50%'
     });
-
+    /* Waypoint 2 */
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
-    });
-
-    /* Mobile navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-
-        nav.slideToggle(200);
-
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
     });
 });
